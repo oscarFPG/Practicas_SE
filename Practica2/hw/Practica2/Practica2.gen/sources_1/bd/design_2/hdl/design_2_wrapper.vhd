17fdc7553ae6f257047cb65b28e34c6a49fbf894
oscar <oscarf1030p@gmail.com>
2025-10-19 11:55:21 +0200

    Registro 3 y leds funcionan

---
+++ Practica2/hw/Practica2/Practica2.gen/sources_1/bd/design_2/hdl/design_2_wrapper.vhd
@@ -0,0 +1,50 @@
+--Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+--Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
+----------------------------------------------------------------------------------
+--Tool Version: Vivado v.2023.1 (win64) Build 3865809 Sun May  7 15:05:29 MDT 2023
+--Date        : Sun Oct 19 11:38:22 2025
+--Host        : PC-OSCAR running 64-bit major release  (build 9200)
+--Command     : generate_target design_2_wrapper.bd
+--Design      : design_2_wrapper
+--Purpose     : IP block netlist
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+entity design_2_wrapper is
+  port (
+    buttons_0 : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    leds_0 : out STD_LOGIC_VECTOR ( 7 downto 0 );
+    reset : in STD_LOGIC;
+    switches_0 : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    sys_clock : in STD_LOGIC;
+    usb_uart_rxd : in STD_LOGIC;
+    usb_uart_txd : out STD_LOGIC
+  );
+end design_2_wrapper;
+
+architecture STRUCTURE of design_2_wrapper is
+  component design_2 is
+  port (
+    reset : in STD_LOGIC;
+    sys_clock : in STD_LOGIC;
+    buttons_0 : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    switches_0 : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    leds_0 : out STD_LOGIC_VECTOR ( 7 downto 0 );
+    usb_uart_rxd : in STD_LOGIC;
+    usb_uart_txd : out STD_LOGIC
+  );
+  end component design_2;
+begin
+design_2_i: component design_2
+     port map (
+      buttons_0(3 downto 0) => buttons_0(3 downto 0),
+      leds_0(7 downto 0) => leds_0(7 downto 0),
+      reset => reset,
+      switches_0(3 downto 0) => switches_0(3 downto 0),
+      sys_clock => sys_clock,
+      usb_uart_rxd => usb_uart_rxd,
+      usb_uart_txd => usb_uart_txd
+    );
+end STRUCTURE;
