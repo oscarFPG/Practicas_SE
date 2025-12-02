26b935ec3c2aeb68537b09437f7cdd87884394bc
oscar <oscarf1030p@gmail.com>
2025-12-02 11:06:57 +0100

    Practica 5 software: main

---
+++ Practica5/sw/design_5_wrapper/platform.tcl
@@ -0,0 +1,19 @@
+# 
+# Usage: To re-create this platform project launch xsct with below options.
+# xsct C:\Users\oscar\OneDrive\Documentos\UCM\5_QUINTO\SE\Practicas_SE\Practica5\sw\design_5_wrapper\platform.tcl
+# 
+# OR launch xsct and run below command.
+# source C:\Users\oscar\OneDrive\Documentos\UCM\5_QUINTO\SE\Practicas_SE\Practica5\sw\design_5_wrapper\platform.tcl
+# 
+# To create the platform in a different location, modify the -out option of "platform create" command.
+# -out option specifies the output directory of the platform project.
+
+platform create -name {design_5_wrapper}\
+-hw {C:\Users\oscar\OneDrive\Documentos\UCM\5_QUINTO\SE\Practicas_SE\Practica5\hw\design_5_wrapper.xsa}\
+-out {C:/Users/oscar/OneDrive/Documentos/UCM/5_QUINTO/SE/Practicas_SE/Practica5/sw}
+
+platform write
+domain create -name {standalone_microblaze_0} -display-name {standalone_microblaze_0} -os {standalone} -proc {microblaze_0} -runtime {cpp} -arch {32-bit} -support-app {peripheral_tests}
+platform generate -domains 
+platform active {design_5_wrapper}
+platform generate -quick
