5f49779903c500e998cd403efabaf90df9fbbfb6
anonymous <149490858+DanLafuente@users.noreply.github.com>
2026-01-20 17:36:53 +0100

    Mezcla punto c
    es la mezcla del menú, maquina de estados, utilizando tus funciones (menos las de los leds que e cambiado)

--- ProyectoFinal/Mezcla_Osni_20262001.c
+++ ProyectoFinal/Mezcla_Osni_20262001.c
@@ -1,86 +1,328 @@
-//define Leds
-#define LED_0 0
-#define LED_1 1
-#define LED_2 2
+/*Dani y Oscar 20_1_2026*/
+#include <stdio.h>
+#include <stdlib.h>
+#include "xparameters.h"
+#include "xil_cache.h"
+#include "xbasic_types.h"
+#include "xil_io.h"
+#include "xil_types.h"
 
-#define RED_CHANNEL LEDS_S00_AXI_SLV_REG0_OFFSET //0
-#define GREEN_CHANNEL LEDS_S00_AXI_SLV_REG1_OFFSET //4
-#define BLUE_CHANNEL LEDS_S00_AXI_SLV_REG2_OFFSET //8
 
-#define READ 0
-#define WRITE 1
+
+// ----------------------- MATRIZ DE PUNTOS ----------------------- //
+#include "matriz.h"
+#define NUM_ROWS 7
+#define NUM_COLS 8
+#define MATRIZ_ADDR XPAR_MATRIZ_0_S00_AXI_BASEADDR
+#define MATRIZ_REG0 MATRIZ_S00_AXI_SLV_REG0_OFFSET
+
+// Constantes para definir los valores de una fila
+// O = Apagado
+// T = Encendido
+#define OOOOO 0
+#define OOOOT 1
+#define OOOTO 2
+#define OOOTT 3
+#define OOTOO 4
+#define OOTOT 5
+#define OOTTO 6
+#define OOTTT 7
+#define OTOOO 8
+#define OTOOT 9
+#define OTOTO 10
+#define OTOTT 11
+#define OTTOO 12
+#define OTTOT 13
+#define OTTTO 14
+#define OTTTT 15
+#define TOOOO 16
+#define TOOOT 17
+#define TOOTO 18
+#define TOOTT 19
+#define TOTOO 20
+#define TOTOT 21
+#define TOTTO 22
+#define TOTTT 23
+#define TTOOO 24
+#define TTOOT 25
+#define TTOTO 26
+#define TTOTT 27
+#define TTTOO 28
+#define TTTOT 29
+#define TTTTO 30
+#define TTTTT 31
+
+
+u32 MATRIZ_escribir(int fila, int columna, u32 valor){
+
+	if(fila < 0 || NUM_ROWS <= fila){ return valor + 1; }
+	if(columna < 0 || NUM_COLS <= columna){ return valor + 1; }
+
+
+	u32 dato = 0;
+	dato = (fila << 11) | (columna << 8) | (valor << 0);
+
+	MATRIZ_mWriteReg(MATRIZ_ADDR, MATRIZ_REG0, dato);
+	return dato;
+}
+// ----------------------- MATRIZ DE PUNTOS ----------------------- //
+
+
+// -----------------------       VGA        ----------------------- //
+/*
+ *	Modificar el valor de la RAM de VGA para modificar el pixel (fila, columna) con el color indicado
+ */
+
+#define VGA_DIM 16
+void VGA_print(u32 fila, u32 columna, u32 color){
+
+	if(0 < fila || VGA_DIM <= fila)
+		return;
+	if(0 < columna || VGA_DIM <= columna)
+		return;
+
+	u32 valor = (columna << 16) + (fila << 12) + color;
+	putfsl(valor, 0);
+}
+
+/*
+ * Metodo para pintar un numero en la posicion (origenRow, origenCol) en la VGA
+ * Cada numero esta representado como se haria en un display de 7 segmentos
+ * Cada dos casillas es un segmento, los espacios entre estos son de una casilla
+ */
+void VGA_printNumber(int origenRow, int origenCol, u32 color, int number){
+
+	switch(number){
+	case 0:
+
+		VGA_print(origenRow, origenCol, color);
+		VGA_print(origenRow, origenCol, color);
+		VGA_print(origenRow, origenCol, color);
+		VGA_print(origenRow, origenCol, color);
+		VGA_print(origenRow, origenCol, color);
+		VGA_print(origenRow, origenCol, color);
+		VGA_print(origenRow, origenCol, color);
+		break;
+
+	case 1:
+
+		VGA_print(origenRow, origenCol, color);
+				VGA_print(origenRow, origenCol, color);
+				VGA_print(origenRow, origenCol, color);
+				VGA_print(origenRow, origenCol, color);
+				VGA_print(origenRow, origenCol, color);
+				VGA_print(origenRow, origenCol, color);
+				VGA_print(origenRow, origenCol, color);
+		break;
+
+	case 2:
+
+		VGA_print(origenRow, origenCol, color);
+				VGA_print(origenRow, origenCol, color);
+				VGA_print(origenRow, origenCol, color);
+				VGA_print(origenRow, origenCol, color);
+				VGA_print(origenRow, origenCol, color);
+				VGA_print(origenRow, origenCol, color);
+				VGA_print(origenRow, origenCol, color);
+		break;
+
+	case 3:
+
+		VGA_print(origenRow, origenCol, color);
+				VGA_print(origenRow, origenCol, color);
+				VGA_print(origenRow, origenCol, color);
+				VGA_print(origenRow, origenCol, color);
+				VGA_print(origenRow, origenCol, color);
+				VGA_print(origenRow, origenCol, color);
+				VGA_print(origenRow, origenCol, color);
+		break;
+
+	case 4:
+
+		VGA_print(origenRow, origenCol, color);
+				VGA_print(origenRow, origenCol, color);
+				VGA_print(origenRow, origenCol, color);
+				VGA_print(origenRow, origenCol, color);
+				VGA_print(origenRow, origenCol, color);
+				VGA_print(origenRow, origenCol, color);
+				VGA_print(origenRow, origenCol, color);
+		break;
+
+	case 5:
+
+		VGA_print(origenRow, origenCol, color);
+		VGA_print(origenRow, origenCol, color);
+		VGA_print(origenRow, origenCol, color);
+		VGA_print(origenRow, origenCol, color);
+		VGA_print(origenRow, origenCol, color);
+		VGA_print(origenRow, origenCol, color);
+		VGA_print(origenRow, origenCol, color);
+		break;
+
+	case 6:
+
+		VGA_print(origenRow, origenCol, color);
+		VGA_print(origenRow, origenCol, color);
+		VGA_print(origenRow, origenCol, color);
+		VGA_print(origenRow, origenCol, color);
+		VGA_print(origenRow, origenCol, color);
+		VGA_print(origenRow, origenCol, color);
+		VGA_print(origenRow, origenCol, color);
+		break;
+
+	case 7:
+
+		VGA_print(origenRow, origenCol, color);
+		VGA_print(origenRow, origenCol, color);
+		VGA_print(origenRow, origenCol, color);
+		VGA_print(origenRow, origenCol, color);
+		VGA_print(origenRow, origenCol, color);
+		VGA_print(origenRow, origenCol, color);
+		VGA_print(origenRow, origenCol, color);
+		break;
+
+	case 8:
+
+		VGA_print(origenRow, origenCol, color);
+		VGA_print(origenRow, origenCol, color);
+		VGA_print(origenRow, origenCol, color);
+		VGA_print(origenRow, origenCol, color);
+		VGA_print(origenRow, origenCol, color);
+		VGA_print(origenRow, origenCol, color);
+		VGA_print(origenRow, origenCol, color);
+		break;
+
+	case 9:
+
+		VGA_print(origenRow, origenCol, color);
+		VGA_print(origenRow, origenCol, color);
+		VGA_print(origenRow, origenCol, color);
+		VGA_print(origenRow, origenCol, color);
+		VGA_print(origenRow, origenCol, color);
+		VGA_print(origenRow, origenCol, color);
+		VGA_print(origenRow, origenCol, color);
+		break;
+
+	default:
+		break;
+	}
+
+}
+// -----------------------       VGA        ----------------------- //
+
+// -----------------------      KEYPAD      ----------------------- //
+#include "keypad.h"
+#define KEYPAD_ADDR XPAR_KEYPAD_0_S00_AXI_BASEADDR
+#define KEYPAD_REG0 KEYPAD_S00_AXI_SLV_REG0_OFFSET
+#define NO_KEY 0xFFFF
+
+/**
+ * Funcion bloqueante para leer un caracter del keypad
+ */
+char KEYPAD_read(){
+
+	KEYPAD_mWriteReg(KEYPAD_ADDR, KEYPAD_REG0, NO_KEY);
+	u32 keycode = NO_KEY;
+
+	while( keycode == NO_KEY ){
+		keycode = KEYPAD_mReadReg(KEYPAD_ADDR, KEYPAD_REG0);
+	}
+
+	if(0 <= keycode && keycode <= 9 )
+		return keycode + '0';
+	else
+		return 'a' + (keycode - 10);
+}
+// -----------------------      KEYPAD      ----------------------- //
+
+// ----------------------        LEDs       ----------------------- //
+#include "led.h"
+#define LED0_ADDR XPAR_LED_0_S00_AXI_BASEADDR
+#define RED_CHANNEL LED_S00_AXI_SLV_REG0_OFFSET
+#define GREEN_CHANNEL LED_S00_AXI_SLV_REG1_OFFSET
+#define BLUE_CHANNEL LED_S00_AXI_SLV_REG2_OFFSET
 
 #define RED 0
 #define GREEN 1
 #define BLUE 2
 
-//define keypad
-#define KEYPAD XPAR_KEYPAD_0_S00_AXI_BASEADDR
+#define LED_UP 255
+#define LED_DOWN 0
 
 
+//Enciende el LED de color colLed
+void EnciendeLed(int colLed) {
 
-
-// ----------------------------- Aux Funtions ----------------------------- //
-u32 fixKeypad(u32 dato) {
-
-	if (dato == 0x1 || dato == 0x5 || dato == 0x9 || dato == 0xC)
-		return dato;
-
-	switch (dato) {
-	case 0x0:
-		dato = 0xE;
+	xil_printf("\n Encendiendo LED \r\n");
+	switch (colLed) {
+	case RED:
+		LED_mWriteReg(LED0_ADDR, RED_CHANNEL, LED_UP);
 		break;
 
-	case 0x2:
-		dato = 0x4;
+	case GREEN:
+		LEDS_mWriteReg(LED0_ADDR, GREEN_CHANNEL, LED_UP);
 		break;
 
-	case 0x3:
-		dato = 0x7;
-		break;
-
-	case 0x4:
-		dato = 0x2;
-		break;
-
-	case 0x6:
-		dato = 0x8;
-		break;
-
-	case 0x7:
-		dato = 0x3;
-		break;
-
-	case 0x8:
-		dato = 0x6;
-		break;
-
-	case 0xA:
-		dato = 0xF;
-		break;
-
-	case 0xB:
-		dato = 0xD;
-		break;
-
-	case 0xD:
-		dato = 0xB;
-		break;
-
-	case 0xE:
-		dato = 0x0;
-		break;
-
-	case 0xF:
-		dato = 0xA;
+	case BLUE:
+		LEDS_mWriteReg(LED0_ADDR, BLUE_CHANNEL, LED_UP);
 		break;
 
 	default:
-		dato = 0x0;
+		xil_printf("\n colLed Corrompido %d \r\n", colLed);
 		break;
 	}
+}
 
-	return dato;
+void ApagaLed(int colLed) {
+
+	xil_printf("\n Apaga LED \r\n");
+	switch (colLed) {
+	case RED:
+		LEDS_mWriteReg(LED0_ADDR, RED_CHANNEL, LED_DOWN);
+		break;
+
+	case GREEN:
+		LEDS_mWriteReg(LED0_ADDR, GREEN_CHANNEL, LED_DOWN);
+		break;
+
+	case BLUE:
+		LEDS_mWriteReg(LED0_ADDR, BLUE_CHANNEL, LED_DOWN);
+		break;
+
+	default:
+		xil_printf("\n colLed Corrompido %d \r\n", colLed);
+		break;
+	}
+}
+// ----------------------        LEDs       ----------------------- //
+
+// ----------------------       PROYECTO    ----------------------- //
+
+//define global parameters
+int NUMRULE = 13;
+int shouldPrint = 1;
+
+// ----------------------------- Aux Funtions ----------------------------- //
+void initialize(){
+
+	// Matriz de puntos vacia
+	for(int f = 0; f < NUM_ROWS; f++){
+		for(int c = 0; c < NUM_COLS; c++){
+			MATRIZ_escribir(f, c, 0);
+		}
+	}
+
+	// LED apagado
+	LEDS_mWriteReg(LED0_ADDR, RED_CHANNEL, LED_DOWN);
+	LEDS_mWriteReg(LED0_ADDR, GREEN_CHANNEL, LED_DOWN);
+	LEDS_mWriteReg(LED0_ADDR, BLUE_CHANNEL, LED_DOWN);
+
+	// VGA en negro
+	for(int f = 0; f < NUM_ROWS; f++){
+		for(int c = 0; c < NUM_COLS; c++){
+			VGA_print(f, c, 0);
+		}
+	}
 }
 
 int SwAcarreo(int acarreo, u32 tecla) {
@@ -127,7 +369,56 @@ int SwAcarreo(int acarreo, u32 tecla) {
 
 	return acarreo;
 }
+
+void escribeMENU(){
+	// Mostrar M en columna 0
+	matriz_escribir(0, 0, TOOOT);
+	matriz_escribir(1, 0, TOOOT);
+	matriz_escribir(2, 0, TOOOT);
+	matriz_escribir(3, 0, TTOTT);
+	matriz_escribir(4, 0, TOTOT);
+	matriz_escribir(5, 0, TOOOT);
+	matriz_escribir(6, 0, TOOOT);
+
+	// Mostrar E en columna 1
+	matriz_escribir(0, 1, TTTTT);
+	matriz_escribir(1, 1, TOOOO);
+	matriz_escribir(2, 1, TOOOO);
+	matriz_escribir(3, 1, TTTTT);
+	matriz_escribir(4, 1, TOOOO);
+	matriz_escribir(5, 1, TOOOO);
+	matriz_escribir(6, 1, TTTTT);
+
+	// Mostrar N en columna 2
+	matriz_escribir(0, 2, TOOOT);
+	matriz_escribir(1, 2, TTOOT);
+	matriz_escribir(2, 2, TOTOT);
+	matriz_escribir(3, 2, TOOTT);
+	matriz_escribir(4, 2, TOOOO);
+	matriz_escribir(5, 2, TOOOO);
+	matriz_escribir(6, 2, TOOOO);
+
+	// Mostrar U en columna 3
+	matriz_escribir(0, 3, TOOOT);
+	matriz_escribir(1, 3, TOOOT);
+	matriz_escribir(2, 3, TOOOT);
+	matriz_escribir(3, 3, TOOOT);
+	matriz_escribir(4, 3, TOOOT);
+	matriz_escribir(5, 3, TOOOT);
+	matriz_escribir(6, 3, TTTTT);
+
+	// Rellenar con 0 la RAM desde la columna 'col' hasta el final
+	int col = 4;
+	for(int i = col; i < NUM_COLS; i++){
+		for(int j = 0; j < NUM_ROWS; j++){
+			matriz_escribir(j, i, OOOOO);
+		}
+	}
+}
 // ----------------------------- Aux Funtions ----------------------------- //
+
+
+
 
 // ----------------------------------------- Maquina de estados ---------------------------------------- //
 
@@ -153,10 +444,7 @@ void LogicMoneda(int* monedas, int op) {
 			shouldPrint = 0;
 		}
 
-		while (tecla == 0xF) {
-			tecla = leerTeclado();
-		}
-
+		tecla = KEYPAD_read();
 		switch (tecla) {
 		case 0x1:
 		case 0x2:
@@ -212,10 +500,8 @@ void Moneda(int* monedas) {
 			xil_printf("\n 1->Aniadir Monedas || 2->Quitar Monedas || B->Salir \r\n");
 			shouldPrint = 0;
 		}
-		while (tecla == 0xF) {
-			tecla = leerTeclado();
-		}
 
+		tecla = KEYPAD_read();
 		switch (tecla) {
 			// Seleccionar opcion
 		case 0x1:
@@ -254,9 +540,7 @@ void CambiaColor(int* colLed) {
 			xil_printf("\n 1->Cambiar a RED || 2->Cambiar a GREEN || 3->Cambiar a BLUE || B->Salir \r\n");
 			shouldPrint = 0;
 		}
-		while (tecla == 0xF) {
-			tecla = leerTeclado();
-		}
+		tecla = KEYPAD_read();
 
 		switch (tecla) {
 			// Seleccionar opcion
@@ -305,9 +589,7 @@ void Color(int* colLedAc, int* colLedFa) {
 			shouldPrint = 0;
 		}
 
-		while (tecla == 0xF) {
-			tecla = leerTeclado();
-		}
+		tecla = KEYPAD_read();
 
 		switch (tecla) {
 			// Seleccionar opcion
@@ -354,9 +636,7 @@ void Ajustes(int* monedas, int* colLedAc, int* colLedFa) {
 			shouldPrint = 0;
 		}
 
-		while (tecla == 0xF) {
-			tecla = leerTeclado();
-		}
+		tecla = KEYPAD_read();
 
 		switch (tecla) {
 			// Seleccionar opcion
@@ -390,49 +670,7 @@ void Ajustes(int* monedas, int* colLedAc, int* colLedFa) {
 
 // ---------------------------------------- La Rule ---------------------------------------- //
 	// ------------------------------ Gira la Rule ------------------------------ //
-void EnciendeLed(int colLed) {
 
-	xil_printf("\n Encendiendo LED \r\n");
-	switch (colLed) {
-	case RED:
-		LEDS_mWriteReg(XPAR_LEDS_1_S00_AXI_BASEADDR, RED_CHANNEL, 0);
-		break;
-
-	case GREEN:
-		LEDS_mWriteReg(XPAR_LEDS_1_S00_AXI_BASEADDR, GREEN_CHANNEL, 0);
-		break;
-
-	case BLUE:
-		LEDS_mWriteReg(XPAR_LEDS_1_S00_AXI_BASEADDR, BLUE_CHANNEL, 0);
-		break;
-
-	default:
-		xil_printf("\n colLed Corrompido %d \r\n", colLed);
-		break;
-	}
-}
-
-void ApagaLed(int colLed) {
-
-	xil_printf("\n Apaga LED \r\n");
-	switch (colLed) {
-	case RED:
-		LEDS_mWriteReg(XPAR_LEDS_1_S00_AXI_BASEADDR, RED_CHANNEL, 255);
-		break;
-
-	case GREEN:
-		LEDS_mWriteReg(XPAR_LEDS_1_S00_AXI_BASEADDR, GREEN_CHANNEL, 255);
-		break;
-
-	case BLUE:
-		LEDS_mWriteReg(XPAR_LEDS_1_S00_AXI_BASEADDR, BLUE_CHANNEL, 255);
-		break;
-
-	default:
-		xil_printf("\n colLed Corrompido %d \r\n", colLed);
-		break;
-	}
-}
 
 void CorreRuleta(int* monedas, int colLedAc, int colLedFa, int numAp, int apuesta) {
 	u32 tecla = 0xF;
@@ -502,10 +740,7 @@ void Apuesta(int* monedas, int colLedAc, int colLedFa, int numAp) {
 			shouldPrint = 0;
 		}
 
-		while (tecla == 0xF) {
-			tecla = leerTeclado();
-		}
-
+		tecla = KEYPAD_read();
 		switch (tecla) {
 		case 0x1:
 		case 0x2:
@@ -566,10 +801,7 @@ void Rule(int* monedas, int colLedAc, int colLedFa) {
 			shouldPrint = 0;
 		}
 
-		while (tecla == 0xF) {
-			tecla = leerTeclado();
-		}
-
+		tecla = KEYPAD_read();
 		switch (tecla) {
 		case 0x1:
 		case 0x2:
@@ -629,10 +861,9 @@ void Menu(int monedas, int colLedAc, int colLedFa) {
 			xil_printf("\n 1->Ajustes || 2->Rule || B->Salir del programa \r\n");
 			shouldPrint = 0;
 		}
-		while (tecla == 0xF) {
-			tecla = leerTeclado();
-		}
+		escribeMENU();
 
+		tecla = KEYPAD_read();
 		switch (tecla) {
 			// Seleccionar opcion
 		case 0x1:
@@ -671,19 +902,10 @@ int main() {
 	Xil_DCacheEnable();
 	//print("--------- Entering main ---------\n\r");
 
-	//LEDS_mWriteReg(XPAR_LEDS_0_S00_AXI_BASEADDR, RED, 255);
-	//LEDS_mWriteReg(XPAR_LEDS_0_S00_AXI_BASEADDR, GREEN, 255);
-	//LEDS_mWriteReg(XPAR_LEDS_0_S00_AXI_BASEADDR, BLUE, 255);
+	initialize();
 
-	LEDS_mWriteReg(XPAR_LEDS_1_S00_AXI_BASEADDR, RED_CHANNEL, 255);
-	LEDS_mWriteReg(XPAR_LEDS_1_S00_AXI_BASEADDR, GREEN_CHANNEL, 255);
-	LEDS_mWriteReg(XPAR_LEDS_1_S00_AXI_BASEADDR, BLUE_CHANNEL, 255);
 
-	//LEDS_mWriteReg(XPAR_LEDS_2_S00_AXI_BASEADDR, RED, 255);
-	//LEDS_mWriteReg(XPAR_LEDS_2_S00_AXI_BASEADDR, GREEN, 255);
-	//LEDS_mWriteReg(XPAR_LEDS_2_S00_AXI_BASEADDR, BLUE, 255);
-
-	int monedas = 0;
+	int monedas = 100;
 	int colLedAc = GREEN;
 	int colLedFa = RED;
 	xil_printf("JUEGO DE LA RULETA\n\r");
@@ -696,3 +918,4 @@ int main() {
 	return 0;
 }
 // --------------------------------- Main --------------------------------- //
+
